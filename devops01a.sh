19745e67c73d4304933fbef0498b084a79c2a4e4
srvemuri <sree.vemuri@gmail.com>
2018-10-14 11:31:54 -0700

    add comments

--- devops01a.sh
+++ devops01a.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# get information about computer
+
+: ' get information about computer
+disk cpu memory network
+'
+echo "Disk information ..."
 df -k
+echo -e "\nCPU information ..."
 cat /proc/cpuinfo | grep process
+echo -e "\nMemory information ..."
 cat /proc/meminfo | grep Mem
+echo -e "\nNetwork information ..."
 ip addr
